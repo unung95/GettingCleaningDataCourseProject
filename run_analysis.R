cd1c7df51b0473fb29d727b15789ec2fcea493ca
unung95 <unung95@yahoo.com>
2014-09-18 13:55:18 +0700

    Create run_analysis.R

---
+++ run_analysis.R
@@ -0,0 +1,6 @@
+###############################
+## Course Project           ###
+## Getting & Cleaning Data  ###
+## Sept 10, 2014            ###
+## unung95@yahoo.com        ###
+###############################
